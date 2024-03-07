860d34f3f64f0d3ba174a7e415835bcc19948c67
Leyian <victor.leyian@student.moringaschool.com>
2024-03-08 02:17:18 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.sorted_word = sorted([letter for letter in word])
+        print(self.sorted_word)
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == self.sorted_word]
+        
